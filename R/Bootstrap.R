08b802dbf9429f82f42d9122d6038fa2fd5dc70d
Duzhe Wang <dwang282@wisc.edu>
2018-03-08 12:20:31 -0600

    Add files via upload

--- R/Bootstrap.R
+++ R/Bootstrap.R
@@ -1,15 +1,21 @@
+## STAT371-L3, Spring 2018
 ## Bootstrap code for second hand smoke example 
 
+
+#########################################################
 ## You can run bootstrap CI using the following chunk. 
+#########################################################
+
 data = c(29,30,53,75,34,21,12,58,117,119,115,134,253,289,287)
 n = length(data)
 xbar = mean(data)
 s = sd(data)
 
-b = 1000 # number of bootstrap samples
-alpha = 0.05 #for 95% CI
+b = 1000       # number of bootstrap samples
+alpha = 0.05   #for 95% CI
 
-resamples = matrix(sample(data, n*b, replace = TRUE), nrow = b, ncol = n) #bootstrap samples collected in matrix
+resamples = matrix(sample(data, n*b, replace = TRUE), nrow = b, ncol = n) 
+#bootstrap samples collected in matrix
 
 t_hat_stat = function(x) {
              (mean(x) - xbar)/(sd(x)/sqrt(length(x))) #define the statistic for calculating t_hat
@@ -20,7 +26,7 @@ t_hats = sort(apply(resamples, 1, t_hat_stat))
 #compute t_hat for each row of resamples. This should yield b values of t_hat
 
 
-t_lower = t_hats[b*alpha/2] #Calculate critical values
+t_lower = t_hats[b*alpha/2]    #Calculate critical values
 t_upper = t_hats[b*(1-alpha/2)] 
 
 CI_lower = xbar-t_upper*s/sqrt(n)
@@ -28,10 +34,16 @@ CI_upper = xbar-t_lower*s/sqrt(n)
 
 print(c(CI_lower, CI_upper))
 
-####################################################################
 
+# the following two lines are used to plot the histogram
+hist(t_hats, freq = F, main = "Bootstrap Distribution of T", xlab = "t", ylab = "density", col = "gray")
+abline(v = c(t_lower, t_upper), col = "red")
 
+#########################################################################
 ## You can also run bootstrap CI using the following different chunk. 
+#########################################################################
+
+rm(list=ls( ))   # clear space
 
 data = c(29,30,53,75,34,21,12,58,117,119,115,134,253,289,287)
 xbar = mean(data)
